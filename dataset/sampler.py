bd71400ce2ad9e3ccef914f8facf7587e2c72ecd
Yacoub Ahmed <dragon.fly1212@yahoo.co.uk>
2019-07-03 11:28:20 +0100

    Sorted sampler a bit to work with relative filepaths better.

--- dataset/sampler.py
+++ dataset/sampler.py
@@ -5,18 +5,21 @@ import json
 
 def get_sample(n=1000):
     sample = {}
-    for lang in os.listdir("./languages_converted"):
+    folder_path = "/".join(__file__.split("/")[:-1])
+    for lang in os.listdir(os.path.join(folder_path, "languages_converted/")):
         if not lang.endswith(".txt"):
             continue
         print(lang[:-4])
         sample[lang[:-4]] = []
-        with open("./languages_converted/" + lang, "r", newline="") as lang_file:
+        with open(
+            os.path.join(folder_path, "languages_converted/", lang), "r", newline=""
+        ) as lang_file:
             lines = lang_file.readlines()
             sample[lang[:-4]] = [random.choice(lines).lower() for j in range(n)]
 
     for key in sample:
         sample[key] = list(map(lambda x: x.replace("\n", ""), sample[key]))
 
-    with open("./test_words.json", "w+") as test_file:
+    with open(os.path.join(folder_path, "test_words.json"), "w+") as test_file:
         test_file.write(json.dumps(sample, indent=4))
 
