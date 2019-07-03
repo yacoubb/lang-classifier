722ed1d5d23f4a68d2ea187818b8318410b27636
Yacoub Ahmed <dragon.fly1212@yahoo.co.uk>
2019-07-03 21:54:57 +0100

    Sampler path issue fixed.

--- model/dataset/sampler.py
+++ model/dataset/sampler.py
@@ -11,16 +11,15 @@ def get_sample(n=1000, languages=utils.get_default_languages()):
     sample = {}
     folder_path = "/".join(__file__.split("/")[:-1])
     for lang in languages:
-        if not os.path.exists(os.path.join(folder_path, (lang + ".txt"))):
+        print(lang)
+        lang_path = os.path.join(folder_path, "languages_converted", (lang + ".txt"))
+        if not os.path.exists(lang_path):
+            print(lang_path, "doesnt exist")
             continue
-        lang = lang + ".txt"
-        print(lang[:-4])
-        sample[lang[:-4]] = []
-        with open(
-            os.path.join(folder_path, "languages_converted/", lang), "r", newline=""
-        ) as lang_file:
+        sample[lang] = []
+        with open(lang_path, "r", newline="") as lang_file:
             lines = lang_file.readlines()
-            sample[lang[:-4]] = [random.choice(lines).lower() for j in range(n)]
+            sample[lang] = [random.choice(lines).lower() for j in range(n)]
 
     for key in sample:
         sample[key] = list(map(lambda x: x.replace("\n", ""), sample[key]))
@@ -28,3 +27,6 @@ def get_sample(n=1000, languages=utils.get_default_languages()):
     with open(os.path.join(folder_path, "test_words.json"), "w+") as test_file:
         test_file.write(json.dumps(sample, indent=4))
 
+
+if __name__ == "__main__":
+    get_sample()
